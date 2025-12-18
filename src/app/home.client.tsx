d8c2e90839a5d20f6fd8bde3635251387fc38950
Gabriel Alm <viniciusgsa99@gmail.com>
2025-12-18 09:04:58 -0300

    fix: corrigir hierarquia entre Server e Client Components no Next.js

---
+++ src/app/home.client.tsx
@@ -0,0 +1,17 @@
+"use client"
+
+import Bottom from "../components/Bottom"
+import Header from "../components/Header"
+import ProductsList from "../components/ProductsList"
+
+export default function HomeClient() {
+  return (
+    <div className="flex text-shops-text-dark min-h-screen items-center justify-center bg-shops-base font-sans">
+      <Header />
+      <div className="w-full h-screen overflow-y-auto pb-10 pt-40 flex flex-col items-center gap-3">
+        <ProductsList />
+      </div>
+      <Bottom />
+    </div>
+  )
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,5 @@
-import Bottom from "../components/Bottom";
-import Header from "../components/Header";
-import ProductsList from "../components/ProductsList";
+import HomeClient from "./home.client"
 
 export default function Home() {
-  return (
-    <div className="flex text-shops-text-dark font min-h-screen items-center justify-center bg-shops-base font-sans">
-      <Header />
-      <div className="w-full h-screen overflow-y-auto pb-10 pt-40 flex flex-col items-center gap-3 ">
-        <ProductsList />
-      </div>
-      <Bottom />
-    </div>
-  );
+  return <HomeClient />
 }
