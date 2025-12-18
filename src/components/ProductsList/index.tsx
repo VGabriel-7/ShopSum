e45842e5d2c90f5a6f37e517249c269ac5de0940
Gabriel Alm <viniciusgsa99@gmail.com>
2025-12-18 09:18:34 -0300

    Fix: corrigindo e unificando css do componente ProductList.

--- src/components/ProductsList/index.tsx
+++ src/components/ProductsList/index.tsx
@@ -5,10 +5,10 @@ import CardItem from "../CardProduct"
 export default function ProductsList() {
   const { products } = useCartStore()
  return (
-  <>
+  <div className="w-full h-screen overflow-y-auto pb-10 pt-40 flex flex-col items-center gap-3">
     {products.map(({id, name, price, quantity}) => (
       <CardItem key={id} id={id as string} name={name}  price={price} quantity={quantity} />
     ))}
-  </>
+  </div>
  )
 }
